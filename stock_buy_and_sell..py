e55615c3454009fb392ca59919bda5f1fabe962b
Sohaib267 <120796948+Sohaib267@users.noreply.github.com>
2023-01-29 00:14:01 +0530

    Add files via upload

---
+++ stock_buy_and_sell..py
@@ -0,0 +1,31 @@
+ 
+def maxProfit(price, start, end):
+    if (end <= start):
+        return 0
+  
+  
+    profit = 0
+ 
+    for i in range(start, end, 1):
+  
+       for j in range(i+1, end+1):
+  
+            if (price[j] > price[i]):
+  
+            
+                curr_profit = price[j] - price[i] +\
+                    maxProfit(price, start, i - 1) + \
+                    maxProfit(price, j + 1, end)
+  
+        
+                profit = max(profit, curr_profit)
+  
+    return profit
+  
+if __name__ == '__main__':
+    price = [100, 180, 502, 310, 40, 535, 500]
+    n = len(price)
+  
+    print(maxProfit(price, 0, n - 1))
+    #The time complexity of this program is O(n^2)
+    #the space complexity is O(n)
